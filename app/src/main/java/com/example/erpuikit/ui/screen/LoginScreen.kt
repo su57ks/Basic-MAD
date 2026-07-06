a672c79713837ecf5e2dd10724892c194e25a2e2
su57ks <samuray.ks@yandex.ru>
2026-07-06 13:38:41 +0300

    LoginS finished ^-^

--- app/src/main/java/com/example/erpuikit/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/erpuikit/ui/screen/LoginScreen.kt
@@ -3,13 +3,25 @@ package com.example.erpuikit.ui.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,6 +33,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,6 +46,15 @@ import com.example.erpuikit.ui.theme.Gray
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
     var login by remember {mutableStateOf("")}
+    var password by remember {mutableStateOf("")}
+    val colors = TextFieldDefaults.colors(
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+        disabledContainerColor = Color.Transparent,
+        focusedIndicatorColor = Color.Gray,
+        unfocusedIndicatorColor = Color.Gray,
+        disabledIndicatorColor = Color.Gray
+    )
 
     Column(
         modifier = Modifier
@@ -39,12 +63,16 @@ fun LoginScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
     ){
-        Image(
-            painter = painterResource(R.drawable.vector),
-            contentDescription = "",
-            modifier = Modifier.fillMaxWidth(),
-            contentScale = ContentScale.FillWidth
-        )
+        Box(
+            modifier = Modifier.padding(start = 40.dp, top = 40.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.vector),
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+        }
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -76,7 +104,59 @@ fun LoginScreen(modifier: Modifier = Modifier) {
             TextField(
                 value = login,
                 onValueChange = {login = it},
-                modifier = Modifier.background(Color.White)
+                modifier = Modifier.fillMaxWidth(),
+                colors = colors
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Password",
+                color = Color.Gray,
+                fontSize = 15.sp
+            )
+            TextField(
+                value = password,
+                onValueChange = {password = it},
+                modifier = Modifier.fillMaxWidth(),
+                colors = colors,
+                visualTransformation = PasswordVisualTransformation(),
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "",
+                        tint = Color.Gray
+                    )
+                }
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Blue,
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = "SIGN IN",
+                         //modifier = Modifier.fillMaxWidth()
+                    )
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = ""
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Forgot password?",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.End
             )
         }
     }
